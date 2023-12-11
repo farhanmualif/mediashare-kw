cc6c565064007291450f0e51e482afb74b6b2828
farhan mualif <94658851+farhanmualif@users.noreply.github.com>
2023-12-11 10:09:45 +0700

    Web: Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+
+app.use(express.static(path.join(__dirname, "src/public")));
+
 app.use("/static", express.static(path.join(__dirname, "src/public")));
 
 app.set("views", path.join(__dirname, "src/views"));
